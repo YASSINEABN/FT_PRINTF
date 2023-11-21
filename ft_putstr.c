ca1d6c3762763aaa747296f426a679d02835df82
Yassine Aboulanouar <146090449+YASSINEABN@users.noreply.github.com>
2023-11-21 10:42:41 +0100

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: yaboulan <yaboulan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:46:34 by yaboulan          #+#    #+#             */
-/*   Updated: 2023/11/18 13:47:51 by yaboulan         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:35:14 by yaboulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ void	ft_putstr(char *txt, int *ptr)
 	int	i;
 
 	i = 0;
-	while (txt[i])
+	if (txt == NULL)
 	{
-		ft_putchar(txt[i], ptr);
-		i++;
+		ft_putstr("(null)", ptr);
+	}
+	else
+	{
+		while (txt[i])
+		{
+			ft_putchar(txt[i], ptr);
+			i++;
+		}
 	}
 }
