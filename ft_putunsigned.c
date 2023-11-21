f259a30e6f9cf39dc30250a657c5e0fc7f35cf8e
Yassine Aboulanouar <146090449+YASSINEABN@users.noreply.github.com>
2023-11-21 10:44:13 +0100

    Update ft_putunsigned.c

--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -6,13 +6,13 @@
 /*   By: yaboulan <yaboulan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:46:41 by yaboulan          #+#    #+#             */
-/*   Updated: 2023/11/18 13:48:56 by yaboulan         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:43:53 by yaboulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putunsigned(unsigned long nb, int *count)
+void	ft_putunsigned(unsigned int nb, int *count)
 {
 	if (nb > 9)
 	{
